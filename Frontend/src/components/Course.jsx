39d1fa6db4c77d0f0539bb9e01602fb180675171
Akanksha Agrawal <kumarianshuagrawal847@gmail.com>
2024-07-08 23:07:12 +0530

    Backend

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -1,28 +1,46 @@
-import React from 'react'
-import Cards from './Cards'
-import list from '../../public/list.json'
-import {Link} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
 function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
   return (
-	<>
-		<div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
-		<div className='mt-28 item-center justify-center text-center'>
-			<h1 className='text-2xl md:text-4xl'>We are delighted to have you here{" "}
-			<span className='text-pink-500'> Here! :)</span></h1>
-			<p className='mt-12'>
-			Books are a fundamental means of recording, disseminating, and preserving knowledge, stories, and information. They have been an essential part of human culture and civilization for centuries, playing a crucial role in education, entertainment, and the spread of ideas. Here are key aspects of books
-			</p>
-			<Link to="/">
-			<button className='mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300'>Back</button>
-			</Link>
-		</div>
-		<div className='mt-12 grid grid-cols-1 md:grid-cols-4'>
-			{list.map((item)=>(<Cards key={item.id} item={item}/>
-			))}
-		</div>
-		</div> 
-	</>
-  )
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+		  Welcome to BookVerse!{" "}
+            <span className="text-pink-500"> Lose yourself in a book, find yourself in a story! :)</span>
+          </h1>
+          <p className="mt-12">
+          Discover a world of stories with BookVerse. Our extensive digital library offers something for everyone, from timeless classics to modern bestsellers. Enjoy seamless reading on any device and connect with a community of book lovers. Start your next literary adventure with us today!
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Course
+export default Course;
